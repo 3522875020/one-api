e9d901e8043151c45d6b769bdbd9662f9472b2f3
linzero <root@linli.eu.org>
2025-03-14 14:46:55 +0800

    feat: 添加新的Gemini嵌入模型到模型列表

--- relay/adaptor/geminiv2/constants.go
+++ relay/adaptor/geminiv2/constants.go
@@ -7,7 +7,7 @@ var ModelList = []string{
 	// "gemma-2-2b-it", "gemma-2-9b-it", "gemma-2-27b-it",
 	"gemini-1.5-flash", "gemini-1.5-flash-8b",
 	"gemini-1.5-pro", "gemini-1.5-pro-experimental",
-	"text-embedding-004", "aqa",
+	"text-embedding-004", "aqa","gemini-embedding-exp-03-07",
 	"gemini-2.0-flash", "gemini-2.0-flash-exp",
 	"gemini-2.0-flash-lite-preview-02-05",
 	"gemini-2.0-flash-thinking-exp-01-21",
